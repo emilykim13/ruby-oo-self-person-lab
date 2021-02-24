2489e093d97a2bd70f09669ab82854785a05f0b0
Cori Blount <heycorihey@icloud.com>
2021-02-24 15:11:14 -0600

    happiness

--- lib/person.rb
+++ lib/person.rb
@@ -11,6 +11,17 @@ class Person
         @hygiene = 8
     end
     
+def happiness=(points)
+    @happiness = points
+    @happiness = 10 if @happiness > 10
+        @happiness = 0 if @happiness < 0
+    end 
+
+def hygiene=(points)
+    @hygiene = points
+    @hygiene = 10 if @hygiene > 10
+    @hygiene = 0 if @hygiene < 0
+end 
 
 end
 
